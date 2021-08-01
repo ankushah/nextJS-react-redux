4c1e3962f2d41aafc5e5c2221e5600a0772c2656
Ankush Shah <ankushshah.jnec@gmail.com>
2021-07-31 18:04:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Sample project for using next js with react and redux 
+this code uses SSR for loading and routing the pages.
+
+Redux for better state management and maintainability
+Next js which handles lazy loading and server side rendering
+React for UI.
+
+
---
+++ server.js
@@ -0,0 +1,25 @@
+const express =  require('express');
+const next = require('next');
+
+const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+app.prepare().then(()=> {
+    const server = express();
+
+    server.get('/page2', (req, res) => {
+        app.render(req, res, '/page2');
+    });
+
+    server.get('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    server.listen(port, (err)=>{
+        if (err) throw err;
+        console.log(`ready on https:localhost:${port}`);
+    });
+})
